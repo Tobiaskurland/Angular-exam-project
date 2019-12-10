4a8107cf32de2cb370c50a560e9e4c11f04f2222
Tobias kurland <torebibeaskka@hotmail.com>
2019-12-10 18:47:05 +0100

    added auth

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCZ4iZv-A7z3jWbasQJ5BZVOgk5yg66ElU",
+    authDomain: "angular-exam-database.firebaseapp.com",
+    databaseURL: "https://angular-exam-database.firebaseio.com",
+    projectId: "angular-exam-database",
+    storageBucket: "angular-exam-database.appspot.com",
+    messagingSenderId: "450581731482",
+    appId: "1:450581731482:web:40fbe1471d0fcb9ea8e723",
+    measurementId: "G-G6VEDT0MYP" 
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCZ4iZv-A7z3jWbasQJ5BZVOgk5yg66ElU",
+    authDomain: "angular-exam-database.firebaseapp.com",
+    databaseURL: "https://angular-exam-database.firebaseio.com",
+    projectId: "angular-exam-database",
+    storageBucket: "angular-exam-database.appspot.com",
+    messagingSenderId: "450581731482",
+    appId: "1:450581731482:web:40fbe1471d0fcb9ea8e723",
+    measurementId: "G-G6VEDT0MYP"  
+  }
+};
