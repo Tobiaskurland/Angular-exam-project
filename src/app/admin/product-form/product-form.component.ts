12e2189e562a9d17e754c74be58e96733e543987
Tobias kurland <torebibeaskka@hotmail.com>
2019-12-15 16:19:19 +0100

    added list of products

--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../category.service';
 import { ProductService } from '../../product.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,12 +12,15 @@ import { ProductService } from '../../product.service';
 export class ProductFormComponent implements OnInit {
   categories$;
 
-  constructor(categoryService: CategoryService, private productService: ProductService) {
+  constructor(private categoryService: CategoryService, 
+              private productService: ProductService, 
+              private router: Router) {
     this.categories$ = categoryService.getCategories();
    }
 
    save(product){
      this.productService.create(product);
+     this.router.navigate(['/admin/products']);
    }
 
   ngOnInit() {
